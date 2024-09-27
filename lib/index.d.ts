8ff71f6bb14239f1cdfb65046abe20b37fd2ec83
June <1601745371@qq.com>
2024-09-28 01:25:14 +0800

    chore: 🚀 补充类型

---
+++ lib/index.d.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: June
+ * @Description: 
+ * @Date: 2024-09-28 01:23:11
+ * @LastEditors: June
+ * @LastEditTime: 2024-09-28 01:23:30
+ */
+declare module 'vue3-justified-layout' {
+  const content: any
+  export default content
+}
